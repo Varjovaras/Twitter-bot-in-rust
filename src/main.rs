dc9bfedafbf024f9fcc1ceca0f269420d75cc7b9
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-11-08 10:42:52 +0200

    remove unused dependencies

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ async fn main() -> anyhow::Result<()> {
 
     // Keep the main thread running
     loop {
-        tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+        tokio::time::sleep(std::time::Duration::from_secs(200)).await;
     }
 }
 
